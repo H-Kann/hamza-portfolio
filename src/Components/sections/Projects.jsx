57f3e3b0a7a444e4427b4a6c760a8094705fb4f7
H-Kann <hamzakannouch@gmail.com>
2026-01-05 01:54:04 +0100

    added projects

--- src/Components/sections/Projects.jsx
+++ src/Components/sections/Projects.jsx
@@ -8,6 +8,9 @@ import flask from "../../assets/flask.svg";
 import ctk from "../../assets/CustomTkinter.ico";
 import rust from "../../assets/rust.svg";
 import java from "../../assets/java.svg";
+import react from "../../assets/react.svg";
+import tailwind from "../../assets/tailwind.svg";
+import daisyui from "../../assets/daisyui.svg";
 export const Projects = () => {
   return (
     <section
@@ -24,8 +27,7 @@ export const Projects = () => {
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-coral2/80 hover:shadow-[0_2px_8px_rgba(59,130,224, 0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">YtDownloader</h3>
               <p className="text-gray-400 mb-4">
-                A simple yet powerful tool that allows users to download YouTube
-                videos quickly and easily.
+                A versatile YouTube video downloader available as both a Windows desktop app and a web app, leveraging yt_dlp for reliable video extraction with support for multiple formats and real-time download progress tracking.
               </p>
 
               <div className="flex flex-wrap gap-2 mb-4">
@@ -46,10 +48,11 @@ export const Projects = () => {
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-coral2/80 hover:shadow-[0_2px_8px_rgba(59,130,224, 0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">miniWall</h3>
               <p className="text-gray-400 mb-4">
-                A minimalist microblogging platform built with JavaScript and
-                MongoDB.
+                MiniWall is a full-stack minimalist microblogging platform inspired by Twitter, built with React, Node.js, and MongoDB, featuring user authentication and full CRUD operations for seamless posting and interaction.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
+                <img src={react} alt="React" />
+                <img src={tailwind} alt="TailwindCSS" />
                 <img src={mongodb} alt="MongoDB" />
                 <img src={restapi} alt="RESTfulAPI" />
                 <img src={js} alt="Javascript" />
@@ -68,9 +71,7 @@ export const Projects = () => {
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-coral2/80 hover:shadow-[0_2px_8px_rgba(59,130,224, 0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">Nes Emulator</h3>
               <p className="text-gray-400 mb-4">
-                A fast, lightweight NES emulator written in Rust. Emulates CPU,
-                PPU, APU and basic mappers with a focus on accuracy and
-                performance.
+                A fully functional NES emulator written in Rust, accurately emulating CPU, PPU, and APU components, complete with real-time controller input and a responsive user interface running at a smooth 60 FPS.
               </p>
 
               <div className="flex flex-wrap gap-2 mb-4">
@@ -90,6 +91,48 @@ export const Projects = () => {
                 <img src={java} alt="Java" />
               </div>
             </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-coral2/80 hover:shadow-[0_2px_8px_rgba(59,130,224, 0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Notes App</h3>
+              <p className="text-gray-400 mb-4">
+                A full-featured notes application built with React, Tailwind CSS, and DaisyUI, allowing users to create, edit, and organize notes through a clean, responsive interface.
+              </p>
+
+              <div className="flex flex-wrap gap-2 mb-4">
+                <img src={react} alt="React" />
+                <img src={tailwind} alt="TailwindCSS" />
+                <img src={daisyui} alt="daisyui" />
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/H-Kann/notes-app"
+                  className="bg-gradient-to-r from-coral1 to-coral2 bg-clip-text text-transparent hover:text-coral2/80 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-coral2/80 hover:shadow-[0_2px_8px_rgba(59,130,224, 0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Shutdown Timer</h3>
+              <p className="text-gray-400 mb-4">
+                A desktop timer utility developed in Java using Atlantafx for modern theming, enabling users to schedule system shutdowns or reminders with precise countdown functionality and an intuitive, visually appealing interface.
+              </p>
+
+              <div className="flex flex-wrap gap-2 mb-4">
+                <img src={java} alt="Java" />
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/H-Kann/ShutdownTimer"
+                  className="bg-gradient-to-r from-coral1 to-coral2 bg-clip-text text-transparent hover:text-coral2/80 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            
           </div>
         </div>
       </RevealOnScroll>
